14374116a503b08ae2c4c775b43639951ad14eb7
Atreya Chandan <atreyachandan786@gmail.com>
2024-03-15 20:41:34 +0530

    Layout destructured

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -15,14 +15,34 @@ const Hero = () => {
 
   return (
     <div>
-      <d2v className="sm:flex min-h-screen w-full sm:m-5 gap-5">
-        <div className="text-center sm:w-[50%] bg-orange-100 m-2">
-          Left Side
+      <div className="sm:flex w-full sm:mt-5 justify-around p-2 gap-3">
+        <div
+          className="flex flex-col 
+         text-center sm:w-[50%] bg-orange-100 p-3"
+        >
+          <div>
+            <h1 className="text-3xl font-bold ">Service Connect Pro</h1>
+          </div>
+          <div className="grid grid-rows-2 grid-flow-col gap-2">
+            <div className="bg-white m-2 p-3 rounded">1</div>
+            <div className="bg-white m-2 p-3 rounded">2</div>
+            <div className="bg-white m-2 p-3 rounded">3</div>
+            <div className="bg-white m-2 p-3 rounded">4</div>
+            <div className="bg-white m-2 p-3 rounded">5</div>
+            <div className="bg-white m-2 p-3 rounded">6</div>
+          </div>
+          <div className="flex justify-around">
+            <div>Rating</div>
+            <div>Users</div>
+          </div>
         </div>
-        <div className="text-center sm:w-[50%] bg-orange-200 m-2">
-          Right Side
+        <div className="text-center sm:w-[50%] bg-orange-200 sm:grid sm:grid-flow-col sm:grid-rows-2 flex flex-col gap-2 p-2">
+          <div className="bg-white rounded">Image-1</div>
+          <div className="bg-white rounded">Image-2</div>
+          <div className="bg-white rounded">Image-3</div>
+          <div className="bg-white rounded">Image-4</div>
         </div>
-      </d2v>
+      </div>
     </div>
   );
 };
--- components/Loader.jsx
+++ components/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => (
   <div className="flex flex-col items-center justify-center min-h-screen">
     <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 sm:h-40 sm:w-40 h-20 w-20"></div>
-    <div className="mt-4 text-black sm:text-2xl text-xl">Loading...</div>
+    <div className="mt-4 text-black sm:text-2xl text-xl">Almost there...</div>
   </div>
 );
 
