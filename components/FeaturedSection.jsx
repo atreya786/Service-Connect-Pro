16d267aec17de8aa0c7792958a4a9d293b65a928
Atreya Chandan <atreyachandan786@gmail.com>
2024-03-19 18:49:08 +0530

    Featured section completed

--- components/FeaturedSection.jsx
+++ components/FeaturedSection.jsx
@@ -11,18 +11,68 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Image from "next/image";
-import img1 from "@/public/card1.jpg";
+import car1 from "@/public/car1.webp";
+import car2 from "@/public/car2.webp";
+import car3 from "@/public/car3.webp";
+import car4 from "@/public/car4.webp";
+import car5 from "@/public/car5.webp";
+import car6 from "@/public/car6.webp";
+
+const Services = [
+  {
+    title: "Companion Care",
+    description:
+      "Provide companionship and emotional support for seniors, engaging in activities such as conversation, games, and outings.",
+    image: car1,
+  },
+  {
+    title: "Personal Care",
+    description:
+      "Assist with activities of daily living including bathing, grooming, dressing, toileting, and mobility assistance.",
+    image: car2,
+  },
+  {
+    title: "Medication Management",
+    description:
+      "Administer medications as prescribed, monitor dosage schedules, and provide reminders for medication refills.",
+    image: car3,
+  },
+  {
+    title: "Meal Preparation",
+    description:
+      "Prepare nutritious meals tailored to dietary needs and preferences, ensuring seniors receive proper nutrition.",
+    image: car4,
+  },
+  {
+    title: "Housekeeping Assistance",
+    description:
+      "Assist with light housekeeping tasks such as laundry, dishes, vacuuming, and tidying up living spaces.",
+    image: car5,
+  },
+  {
+    title: "Transportation Services",
+    description:
+      "Provide transportation for medical appointments, grocery shopping, errands, and social outings, ensuring seniors remain active and engaged.",
+    image: car6,
+  },
+];
 
 export default function FeaturedSection() {
   return (
     <Carousel className="w-full max-w-[90%] repeat-infinite">
       <CarouselContent className="-ml-1">
-        {Array.from({ length: 6 }).map((_, index) => (
+        {Services.map((service, index) => (
           <CarouselItem key={index} className="pl-1 sm:basis-1/2 md:basis-1/3">
             <div className="p-1">
               <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <Image src={img1} alt="" className="rounded " />
+                <CardContent className="flex gap-2 flex-col aspect-square items-center justify-center p-2">
+                  <Image
+                    src={service.image}
+                    alt={service.title}
+                    className="rounded w-full h-auto"
+                  />
+                  <h3 className="text-xl font-semibold">{service.title}</h3>
+                  <p className="text-sm text-center">{service.description}</p>
                 </CardContent>
               </Card>
             </div>
