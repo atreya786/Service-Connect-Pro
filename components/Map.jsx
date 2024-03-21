379c48dcafaa32a90162afbcdb6938ed7af1ec68
Atreya Chandan <atreyachandan786@gmail.com>
2024-03-22 01:11:41 +0530

    Map component set up completed

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Map = () => {
-  return <div>Map</div>;
+  return (
+    <MapContainer
+      center={[20.296059, 85.824539]}
+      zoom={15}
+      scrollWheelZoom={false}
+      style={{ height: "400px", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[20.296059, 85.824539]}>
+        <Popup>Bhubaneswar</Popup>
+      </Marker>
+    </MapContainer>
+  );
 };
 
 export default Map;
